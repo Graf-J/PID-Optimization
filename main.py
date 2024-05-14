36fc8950d471875900ecae577b863ab2fe478087
Graf-J <johannes.graf.1908@gmail.com>
2024-05-14 23:40:11 +0200

    Add Load-Genome Functionality with ID

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from src.visualization import VisualizationFactory, VisualizationType
 def main():
     # visualization = VisualizationFactory.create_visualization(VisualizationType.KEYBOARD, log_data=True)
 
-    kp = -14.3617
-    ki = -0.2086
-    kd = -328.848
+    kp = -10.3617
+    ki = -0.1086
+    kd = -250.848
     visualization = VisualizationFactory.create_visualization(VisualizationType.PID, kp=kp, ki=ki, kd=kd)
     visualization.run()
 
