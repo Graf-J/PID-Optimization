8a390c19c6f2a47a6558a7f59c5aa17a4dce338b
Graf-J <johannes.graf.1908@gmail.com>
2024-06-28 13:39:38 +0200

    Add Optimization to find Parameters

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from src.visualization import VisualizationFactory, VisualizationType
 
 
 def main():
-    visualization = VisualizationFactory.create_visualization(VisualizationType.KEYBOARD, log_data=True)
+    # visualization = VisualizationFactory.create_visualization(VisualizationType.KEYBOARD, log_data=True)
 
-    # kp = -10.3617
-    # ki = -0.1086
-    # kd = -250.848
-    # visualization = VisualizationFactory.create_visualization(VisualizationType.PID, kp=kp, ki=ki, kd=kd)
+    kp = -10.3617
+    ki = -0.1086
+    kd = -250.848
+    visualization = VisualizationFactory.create_visualization(VisualizationType.PID, kp=kp, ki=ki, kd=kd)
     visualization.run()
 
 
